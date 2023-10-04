d3f7390f09a3805d9d2a68015c5266ba2b1baf16
Archana Inapudi <ainapudi@amazon.com>
2023-10-03 22:27:07 -0700

    removed commented code

--- streamlit-testing/streamlit_app.py
+++ streamlit-testing/streamlit_app.py
@@ -16,6 +16,7 @@ def generate_response(input_text):
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
+  openai_api_key = os.environ["OPENAI_API_KEY"]
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
