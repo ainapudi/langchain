16761cae89da88afcc6c680f5b33607d95f15e0d
Archana Inapudi <ainapudi@amazon.com>
2023-10-03 22:14:37 -0700

    add environment variable

--- streamlit-testing/streamlit_app.py
+++ streamlit-testing/streamlit_app.py
@@ -1,8 +1,13 @@
+import os
 import streamlit as st
 from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 openai_api_key = st.write("Secret Key", st.secrets["openai_secret_key"])
+st.write(
+    "Has environment variables been set:",
+    os.environ["openai_secret_key"] == st.secrets["openai_secret_key"],
+)
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -11,7 +16,6 @@ def generate_response(input_text):
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
-  st.text_area('My Key:', openai_api_key)
   # if not openai_api_key.startswith('sk-'):
   #   st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted:
