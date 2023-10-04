4fe0fe05d03400ba86c9f73fdeede6a50c5eb11e
Archana Inapudi <ainapudi@amazon.com>
2023-10-03 22:10:17 -0700

    using secrets

--- streamlit-testing/streamlit_app.py
+++ streamlit-testing/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
-openai_api_key = st.sidebar.text_input('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')
+openai_api_key = st.write("Secret Key", st.secrets["openai_secret_key"])
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -11,6 +11,7 @@ def generate_response(input_text):
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
+  st.text_area('My Key:', openai_api_key)
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
